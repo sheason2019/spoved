b915f63edc49960b5b6c2e9cbf4d3fb1d22985e5
sheason2019 <811627877@qq.com>
2023-02-18 20:19:01 +0800

    feat: 使用多入口模式对项目进行初始化

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
-cd /code
+CURRENT_DIR=$(dirname $0)
 
+cd $CURRENT_DIR
 
 export GOPROXY=https://proxy.golang.com.cn,direct
 export GOSUMDB=off
 
-if [ "$PRODUCT"z == "true"z ]; then
+if [ "$PRODUCT"z = "true"z ]; then
   echo "build production"
-  go build -o spoved -tags product .
+  go build -o spoved -tags product ./cmd/spoved
 else
   echo "build development"
-  go build -o spoved .
+  go build -o spoved ./cmd/spoved
 fi
--- cmd/initial/main.go
+++ cmd/initial/main.go
@@ -4,14 +4,9 @@ import (
 	"context"
 
 	"github.com/sheason2019/spoved/libs/initial"
-	"github.com/sheason2019/spoved/libs/router"
 )
 
 func main() {
 	ctx := context.TODO()
 	initial.Initial(ctx)
-
-	r := router.SetupRouter()
-
-	r.Run(":80")
 }
